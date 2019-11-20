0c144bb9f04b8b4c4d1652c1c63c0d0f7357f897
Chris <c.p.admiraal@student.utwente.nl>
2019-11-20 16:47:08 +0100

    Added support for open HTML questions The answers will be shown in code and in actual HTML representation Also added CSP; so no images / scripts from other websites will be loaded

--- www/admin/quiz.php
+++ www/admin/quiz.php
@@ -1,63 +1,89 @@
-<?php 
-    session_start();
-    include("../helpers/session.php");
-    include("../helpers/db.php");
+<?php
+session_start();
+include("../helpers/session.php");
+include("../helpers/db.php");
 
-    if (!loggedin()) {
-        header("location: /admin/login.php");
-        exit;
-    } 
+if (!loggedin()) {
+    header("location: /admin/login.php");
+    exit;
+}
 
-    $quiz_id = $_GET["id"];
-    if (!($title = get_quiz_title($quiz_id))) {
-        http_response_code(404);
-        echo "This quiz doesn't exist.";
-        exit;
+$quiz_id = $_GET["id"];
+if (!($title = get_quiz_title($quiz_id))) {
+    http_response_code(404);
+    echo "This quiz doesn't exist.";
+    exit;
+}
+
+$errors = array();
+if (!empty($_POST)) {
+    $question = $_POST["question"];
+    $correct = $_POST["correct"];
+
+    if (empty($question)) {
+        $errors[] = "Vul een vraag in.";
+    }
+    if (empty($correct)) {
+        $errors[] = "Kies het juiste antwoord.";
     }
 
-    $errors = array();
-    if (!empty($_POST)) {
-        $question = $_POST["question"];
-        $answers = array($_POST["answer1"], $_POST["answer2"], $_POST["answer3"], $_POST["answer4"]);
-        $correct = $_POST["correct"];
 
-        if (empty($question)) { $errors[] = "Vul een vraag in."; }
-        if (empty($correct)) { $errors[] = "Kies het juiste antwoord."; }
+    if (empty($errors)) {
 
-        if (empty($errors)) {
+        if ($_POST["type"] === "Voeg meerkeuze vraag toe") {
+            // Mulitple choice
+            $answers = array($_POST["answer1"], $_POST["answer2"], $_POST["answer3"], $_POST["answer4"]);
+
             $q = array(
+                "type" => "mc",
                 "question" => $question,
                 "answers" => $answers,
                 "correct" => $correct);
             create_question_for_quiz($quiz_id, json_encode($q));
+        } else {
+            // HTML question
+            $q = array(
+                "type" => "html",
+                "question" => $question,
+                "correct" => $correct);
+            create_question_for_quiz($quiz_id, json_encode($q));
         }
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="nl">
 <head>
     <title>Admin - Informatiquiz</title>
-    <link rel="stylesheet" type="text/css" href="/style.css" />
+    <link rel="stylesheet" type="text/css" href="/style.css"/>
 </head>
 <body>
-    <h1><?php echo $title; ?></h1>
-    <form method="POST" action="quizrun.php">
-        <input type="hidden" name="quiz_id" value="<?php echo $quiz_id ?>" />
-        <input type="submit" value="Start quiz" />
-    </form>
-    <?php foreach (get_questions_for_quiz($quiz_id) as $quiz) {
-        $id = $quiz["id"];
-        $question = $quiz["question"];
-        echo "$question<br/>"; 
-    } ?>
+<h1><?php echo $title; ?></h1>
+<form method="POST" action="quizrun.php">
+    <input type="hidden" name="quiz_id" value="<?php echo $quiz_id ?>"/>
+    <input type="submit" value="Start quiz"/>
+</form>
+<?php foreach (get_questions_for_quiz($quiz_id) as $quiz) {
+    $id = $quiz["id"];
+    $question = $quiz["question"];
+    echo "$question<br/>";
+} ?>
+
+<h2>Nieuwe vraag</h2>
+
+
+<fieldset>
+    <legend>Meerkeuze vraag</legend>
     <form method="POST">
-        <h2>Nieuwe vraag</h2>
+
         <ul class="errors">
             <?php foreach ($errors as $err) {
                 echo "<li>$err</li>";
             } ?>
         </ul>
+
+
         <fieldset>
             <legend>Vraag</legend>
             <textarea name="question"></textarea>
@@ -76,7 +102,28 @@
             <input type="radio" name="correct" value="3">3</input>
             <input type="radio" name="correct" value="4">4</input>
         </fieldset>
-        <input type="submit" value="Maak quiz" />
+        <input type="submit" name="type" value="Voeg meerkeuze vraag toe"/>
     </form>
+
+</fieldset>
+<br>
+
+<!-- Question where student has to answer with HTML code -->
+<fieldset>
+    <legend>Open vraag (HTML)</legend>
+    <form method="POST">
+
+        <fieldset>
+            <legend>Vraag</legend>
+            <textarea name="question"></textarea>
+        </fieldset>
+        <fieldset>
+            <legend>Goede HTML antwoord</legend>
+            <input type="text" name="correct">
+        </fieldset>
+        <input type="submit" name="type" value="Voeg open vraag (HTML) toe"/>
+    </form>
+</fieldset>
+
 </body>
 </html>
--- www/admin/quizrun.php
+++ www/admin/quizrun.php
@@ -1,69 +1,82 @@
-<?php 
-    session_start();
-    include("../helpers/session.php");
-    include("../helpers/db.php");
-    include("../helpers/Parsedown.php");
+<?php
+session_start();
+include("../helpers/session.php");
+include("../helpers/db.php");
+include("../helpers/Parsedown.php");
 
-    if (!loggedin()) {
-        header("location: /admin/login.php");
-        exit;
-    }
+if (!loggedin()) {
+    header("location: /admin/login.php");
+    exit;
+}
 
-    if (!empty($_POST)) {
-        $quiz_id = $_POST["quiz_id"];
-        $access_code = random_int(10000, 99999);
-        $id = create_quizrun_for_quiz($quiz_id, $access_code, true);
-        header("location: /admin/quizrun.php?id=$id");
-        exit;
-    }
+if (!empty($_POST)) {
+    $quiz_id = $_POST["quiz_id"];
+    $access_code = random_int(10000, 99999);
+    $id = create_quizrun_for_quiz($quiz_id, $access_code, true);
+    header("location: /admin/quizrun.php?id=$id");
+    exit;
+}
 
-    $quizrun_id = $_GET["id"];
-    if (!($quizrun = get_quizrun($quizrun_id))) {
-        http_response_code(404);
-        echo "This quizrun doesn't exist.";
-        exit;
-    }
+$quizrun_id = $_GET["id"];
+if (!($quizrun = get_quizrun($quizrun_id))) {
+    http_response_code(404);
+    echo "This quizrun doesn't exist.";
+    exit;
+}
 
-    $questions = get_questions_for_quizrun($quizrun_id);
-    
-    $parsedown = new Parsedown();
+$questions = get_questions_for_quizrun($quizrun_id);
+
+$parsedown = new Parsedown();
 ?>
 
 <!DOCTYPE html>
 <html lang="nl">
 <head>
     <title>Admin - Informatiquiz</title>
-    <link rel="stylesheet" type="text/css" href="/style.css" />
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'">
+    <link rel="stylesheet" type="text/css" href="/style.css"/>
 </head>
 <body class="admin-run">
-    <h1 class="access-code">Code: <?php echo $quizrun["access_code"]; ?></h1>
-    <?php if ($quizrun["active"]) {
-        if (!!($current_question = get_question($quizrun["current_question"]))) {
-            $question = json_decode($current_question); ?>
-            <div class="question">
-                <?php echo $parsedown->text($question->question); ?>
-                <ol>
-                    <?php for ($i=0; $i < count($question->answers); $i++) { 
-                        echo "<li>" . $parsedown->line($question->answers[$i]) . "</li>";    
-                    } ?>
-                </ol>
-            </div>
-        <?php } 
-        if (!!($answers = get_answers_for_quizrun_question($quizrun["id"], $quizrun["current_question"]))) {
-            echo "<h2>Resultaten</h2><ul>";
+<h1 class="access-code">Code: <?php echo $quizrun["access_code"]; ?></h1>
+<?php if ($quizrun["active"]) {
+    if (!!($current_question = get_question($quizrun["current_question"]))) {
+        $question = json_decode($current_question); ?>
+        <div class="question">
+        <?php echo $parsedown->text($question->question); ?>
+        <ol>
+            <?php
+            if ($question->type === "mc") {
+                for ($i = 0; $i < count($question->answers); $i++) {
+                    echo "<li>" . $parsedown->line($question->answers[$i]) . "</li>";
+                }
+            }
+            ?>
+        </ol>
+        </div><?php }
+    if (!!($answers = get_answers_for_quizrun_question($quizrun["id"], $quizrun["current_question"]))) {
+        echo "<h2>Resultaten</h2>";
+
+        // Check if mc:
+        if ($question->type === "mc") {
+            echo "<ul>";
             foreach ($answers as $a) {
                 echo "<li>" . (intval($a["answer"]) + 1) . ": " . $a["count"] . "</li>";
             }
             echo "</ul>";
+        } elseif ($question->type === "html") {
+            foreach ($answers as $a) {
+                echo "<fieldset> <legend>Antwoord </legend> HTML Code: <br> <code>" . htmlspecialchars($a["answer"]) . "</code><br><br> Browser: <br>" . $a["answer"] . "</fieldset>";
+            }
         }
-        ?>
+    }
+    ?>
 
-        <form method="POST" action="/admin/quizrun_next.php">
-            <input type="hidden" name="quizrun" value="<?php echo $quizrun_id ?>" />
-            <input type="submit" value="volgende" />
-        </form>
-    <?php } else { ?>
-        <h1>Einde</h1>
-    <?php } ?>
+    <form method="POST" action="/admin/quizrun_next.php">
+        <input type="hidden" name="quizrun" value="<?php echo $quizrun_id ?>"/>
+        <input type="submit" value="volgende"/>
+    </form>
+<?php } else { ?>
+    <h1>Einde</h1>
+<?php } ?>
 </body>
 </html>
